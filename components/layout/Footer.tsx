5a71471f2652f88ccf60a792622442e530b1d40d
Unknown <bushmango@gmail.com>
2020-05-07 14:16:23 -0500

    typo fix

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <div className={css.footer}>
       <div>&copy; 2015-2020 Stevie Bushman and LGBT Books to Prisoners</div>
       <div>
-        For support / comments / inqueries contact : Stevie Bushman &ndash;
+        For support / comments / inquiries contact : Stevie Bushman &ndash;
         stevie@steviebushman.com &ndash; 1.414.510.2140
       </div>
     </div>
