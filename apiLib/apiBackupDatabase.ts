a01f0764a75f334b7accb40bfe2b4e8cb2135ef6
Steve Bushman <steve@stevebushman.com>
2024-03-19 13:15:23 -0500

    zip file download

--- apiLib/apiBackupDatabase.ts
+++ apiLib/apiBackupDatabase.ts
@@ -1,10 +1,7 @@
+import AdmZip from 'adm-zip'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { l } from '../common/lib/lodash'
-import { apiRegister } from './api'
-import { postJson } from './lib/apiUtil'
-import { db } from './lib/databaseB2P'
 import { checkToken } from './apiAccount'
-
+import { db } from './lib/databaseB2P'
 const table = 'b2p_people_v5'
 
 export function installAll() {
@@ -27,6 +24,8 @@ export async function runReport_backupDatabase(
     return
   }
 
+  // TODO: download to s3 or zip or something as vercel has a 4.5 mb limit
+
   let limit = 9999999999999 //500000
   let organization = user?.organization
 
@@ -41,13 +40,32 @@ export async function runReport_backupDatabase(
 
   let results = await query
 
-  // Send results as a downloadable json file
-  res.setHeader(
-    'Content-Disposition',
-    'attachment; filename=backup-lgbt-b2p-database.txt',
-  )
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(results, null, 2))
+  const sendAsZip = true
+  if (sendAsZip) {
+    var zip = new AdmZip()
+    // add local file
+    zip.addFile(
+      'backup-lgbt-b2p-database.json',
+      Buffer.from(JSON.stringify(results), 'utf8'),
+    )
+    // get everything as a buffer
+    var zipFileContents = zip.toBuffer()
+    const fileName = 'backup-lgbt-b2p-database.zip'
+    const fileType = 'application/zip'
+    res.writeHead(200, {
+      'Content-Disposition': `attachment; filename="${fileName}"`,
+      'Content-Type': fileType,
+    })
+    return res.end(zipFileContents)
+  } else {
+    // Send results as a downloadable json file
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename=backup-lgbt-b2p-database.txt',
+    )
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(results, null, 2))
+  }
 
   //    return {
   //    records: l.map(results, (c) => c),
